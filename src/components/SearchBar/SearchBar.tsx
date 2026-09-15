524f1af8223af6dae761456cbeb4dafabf5bd1bd
Andrew Dyoma <heelmyt12@gmail.com>
2026-09-15 22:04:53 +0300

    fix bugs

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,49 +1,42 @@
+import React, { useRef } from "react";
 import toast from "react-hot-toast";
-import styles from "./SearchBar.module.css";
+import css from "./SearchBar.module.css";
 
-export interface SearchBarProps {
+interface SearchBarProps {
   onSubmit: (query: string) => void;
 }
 
-export default function SearchBar({ onSubmit }: SearchBarProps) {
-  const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const query = (formData.get("query") as string).trim();
+export const SearchBar: React.FC<SearchBarProps> = ({ onSubmit }) => {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const handleSubmit = (formData: FormData) => {
+    const query = (formData.get("query") as string)?.trim();
 
     if (!query) {
-      toast.error("Please enter your search query.");
+      toast.error("Будь ласка, введіть пошуковий запит!");
       return;
     }
 
     onSubmit(query);
+
+    formRef.current?.reset();
   };
 
   return (
-    <header className={styles.header}>
-      <div className={styles.container}>
-        <a
-          className={styles.link}
-          href="https://www.themoviedb.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by TMDB
-        </a>
-        <form className={styles.form} onSubmit={handleSubmit}>
-          <input
-            className={styles.input}
-            type="text"
-            name="query"
-            autoComplete="off"
-            placeholder="Search movies..."
-            autoFocus
-          />
-          <button className={styles.button} type="submit">
-            Search
-          </button>
-        </form>
-      </div>
+    <header className={css.searchHeader}>
+      <form ref={formRef} action={handleSubmit} className={css.searchForm}>
+        <input
+          type="text"
+          name="query"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          className={css.searchInput}
+        />
+        <button type="submit" className={css.searchButton}>
+          Search
+        </button>
+      </form>
     </header>
   );
-}
+};
